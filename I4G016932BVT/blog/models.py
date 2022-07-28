c5d602aba81f10a704d151969e2003f953a1e3a7
Bright Chigozie <goziedbrightest@gmail.com>
2022-07-28 05:50:44 -0700

    Done

--- I4G016932BVT/blog/models.py
+++ I4G016932BVT/blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class post(models.Model):
+    author = models.ForeignKey(User, related_name='author', on_delete= models.CASCADE)
     Title  = models.CharField(max_length=200)
     Text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(auto_now_add=True)
-    
